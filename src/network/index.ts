e1762bdd4ab4da84d5f9a4dd2e493ef95ada862a
naftalimurgor <murgornaftali@gmail.com>
2023-10-22 17:33:27 +0300

    [WIP]: add transaction broadcast and sign functions

---
+++ src/network/index.ts
@@ -0,0 +1,5 @@
+// use standard http request to broadcast the transaction
+// but first: create the tx object, sign
+// afterwards broadcast: check on ethereums ethers
+// mvsd-rawsign implementation
+// further use the wallet/broadcast to create a simple wallet online
